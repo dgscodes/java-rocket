0e8ed704f1478173217ec52c4cae145526aa9ade
dgscodes <dodowork123@gmail.com>
2022-02-27 17:40:08 +0200

    cycle test

---
+++ script.js
@@ -0,0 +1,17 @@
+This is so cool
+var car = {
+
+	make: 'BMW'
+	colour: 'red',
+	model: '3 Series',
+	isTurnedon: false,
+	numberofSeats: 4,
+
+	seats: [ 
+
+	'seat 1',
+	'seat 2',
+	'seat 3',
+	'seat 4']
+
+};
---
+++ untitled.js
@@ -0,0 +1,18 @@
+var car = {
+
+	make: 'BMW'
+	colour: 'red',
+	model: '3 Series',
+	isTurnedon: false,
+	numberofSeats: 4,
+
+	seats: [ 
+
+	'seat 1',
+	'seat 2',
+	'seat 3',
+	'seat 4'
+	
+	]
+
+};
